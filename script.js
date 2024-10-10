1a8967ff14fa3809e8e721402d678192245be02a
cogolloguzmanesteban@gmail.com <cogolloguzmanesteban@gmail.com>
2024-10-10 13:54:09 -0500

    Primer commit

---
+++ script.js
@@ -0,0 +1,18 @@
+// /*prompt()Funcion que permite capturar datos*/
+// // console.log("Hello World")/*Funcion que permite mostrar datos*/ 
+// // prompt("Hola dobleSetenta ingrese su numero de celular")
+
+// // /*Variables*/
+
+// var nombre=prompt("Nombre, por favor: ");
+// var apellido=prompt("Apellido, Cv: ");
+// console.log("Hola,"+ nombre + apellido)
+
+// var nombre=prompt("Nombre de la persona a insultar: ")
+// console.log(nombre, "Que hijueputa tan bobo ome")
+
+
+var num1=Number(prompt("Ingresa tu numero 1 : "))
+var num2=parseInt(prompt("ingrese tu numero 2: "))
+var suma=num1+num2
+console.log(suma);
