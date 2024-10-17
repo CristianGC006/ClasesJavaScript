c0653dc35b7dc636305a32a60fcea35702858f1f
CristianGC006 <cogolloguzmanesteban@gmail.com>
2024-10-17 15:50:24 -0500

    Primer commit

--- claseTres/claseTres.js
+++ claseTres/claseTres.js
@@ -1,5 +1,16 @@
 /*Funciones declarativas*/
 
-function funcionDeclaratuva(){
-    git 
+function funcionDeclarativa(){
+    let nombre='Gustavo Petro';
+    let edad=59
+    console.log("Soy "+nombre+' Tengo '+edad+" Esta es una funcion declarativa")
 }
+funcionDeclarativa()
+
+/*Funcion por expresion */
+let funcionDeExpresion=function(){
+    let nombre='Gustavo Petro';
+    let edad=59
+    console.log("Soy "+nombre+' Tengo '+edad +" Y Estoy aprendiendo funciones"+" Esto es una funcion de expresion")
+}
+funcionDeExpresion()
